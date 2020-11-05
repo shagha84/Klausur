ae751d9f93d07346a1225f612d4f84bf24f639d4
Shagha <wi19b085@technikum-wien.at>
2020-11-05 09:40:39 +0100

    Projektdateien hinzufügen.

---
+++ Klausur/SpecFlowFeature1.feature
@@ -0,0 +1,20 @@
+﻿Feature: SpecFlowFeature1
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario: calculate the sine
+	Given the radian is Pi
+	When the sine is clicked
+	Then the result should be 0
+
+Scenario: calculate the cosine
+    Given the radian is Pi
+	When the cosine is clicked
+	Then the result should be -1
+
+Scenario: calculate the tangens
+    Given the radian is Pi
+	When the tangens is clicked
+	Then the result should be 0
---
+++ Klausur/SpecFlowFeature1Steps.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace Klausur
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        double rd, result;
+
+        [Given(@"the radian is Pi")]
+        public void GivenTheRadianIsPi()
+        {
+            rd = Math.PI;
+        }
+
+        [When(@"the sine is clicked")]
+        public void WhenTheSineIsClicked()
+        {
+            result = Math.Sin(rd);
+        }
+
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(double p0)
+        {
+            var expected = p0;
+            var actual = result;
+            Assert.Equal(expected, actual, 5);
+        }
+
+        [When(@"the cosine is clicked")]
+        public void WhenTheCosineIsClicked()
+        {
+            result = Math.Cos(rd);
+        }
+
+        [When(@"the tangens is clicked")]
+        public void WhenTheTangensIsClicked()
+        {
+            result = Math.Tan(rd);
+        }
+    }
+}
